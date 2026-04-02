579e9acfeb7eed0691c2f9ab2658ed58ab6a9d15
Mohammad Zaid <zaidcs@student.iul.ac.in>
2026-04-02 20:11:17 +0530

    Deleting some files that are not needed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,8 @@ import RevenueLeak from "./components/RevenueLeak";
 
 export default function Home() {
   return (
-  <>
+    <>
       {/* <Navbar /> */}
-      <div className="pointer-events-none absolute -bottom-24 -left-24 h-144 w-xl rounded-full bg-linear-to-tr from-blue-400/40 via-blue-500/30 to-transparent blur-3xl max-w-screen"></div>
       <Hero />
      
       <RevealAnimation delay={0.2}>
